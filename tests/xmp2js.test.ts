4d926d662df0a270811853216e5dfd57a56c63a8
Aidin Gharibnavaz <aidin@aidinhut.com>
2025-01-13 10:49:07 +1300

    A test with multiple languages

--- tests/xmp2js.test.ts
+++ tests/xmp2js.test.ts
@@ -2,13 +2,18 @@ import { it, expect } from '@jest/globals'
 
 import { xmp2js } from '../src/index'
 import { iptcStd2024Xmp, iptcStd2024Js } from './data/iptc-std-2024.1'
+import { digikamMultiLangXmp, digikamMultiLangJs } from './data/digikam-multi-langs'
 
 it('should read IPTC std 2024.1 xmp data', () => {
   // IPTC standard provides a JPEG file with this XMP data.
 
   const actualJs = xmp2js(iptcStd2024Xmp)
 
-  console.log(JSON.stringify(actualJs, null, 2))
-
   expect(actualJs).toEqual(iptcStd2024Js)
+})
+
+it('should read XMP with multiple languages', () => {
+  const actualJs = xmp2js(digikamMultiLangXmp)
+
+  expect(actualJs).toEqual(digikamMultiLangJs)
 })
